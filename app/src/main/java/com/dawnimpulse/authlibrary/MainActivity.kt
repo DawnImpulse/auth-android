43ab2b7582febaf7c8387883fff0624efa8e7bd5
DawnImpulse <dawnimpulse@gmail.com>
2018-01-23 21:02:33 +0530

    Initial

---
+++ app/src/main/java/com/dawnimpulse/authlibrary/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.dawnimpulse.authlibrary
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.dawnimpulse.auth.Test
+import com.dawnimpulse.library.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        id1.text = Test.first().toString()
+    }
+}
---
+++ auth/src/main/java/com/dawnimpulse/auth/Test.kt
@@ -0,0 +1,20 @@
+package com.dawnimpulse.auth
+
+import com.google.firebase.auth.FirebaseAuth
+
+/**
+ * Created by Saksham on 2018 01 18
+ * Last Branch Update - v
+ * Updates :
+ * Saksham - 2018 01 18 - v - Initial
+ */
+object Test {
+    fun first(): Int {
+        if (FirebaseAuth.getInstance().currentUser != null) {
+
+        } else {
+            return 0
+        }
+        return 1
+    }
+}
